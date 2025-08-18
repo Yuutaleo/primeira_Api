919bf352321e3a88350545063b4df5d99258c697
Yuutaleo <leo.yuta2004@gmail.com>
2025-08-18 17:41:38 +0000

    Comite Aula 3

--- src/main/java/application/repository/TarefaRepository.java
+++ src/main/java/application/repository/TarefaRepository.java
@@ -1,9 +1,10 @@
 package application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import application.model.Tarefa;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
 }
+
+//JpaRepository 
